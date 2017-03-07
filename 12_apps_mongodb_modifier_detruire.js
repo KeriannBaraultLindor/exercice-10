ca513d421b815fd0621530a09da1f19cb8ed0e87
Keriann Barault-Lindor <keriannbl@hotmail.com>
2017-03-07 18:07:05 -0500

    premier (et seul) commit pour l'exercice 10

--- 12_apps_mongodb_modifier_detruire.js
+++ 12_apps_mongodb_modifier_detruire.js
@@ -2,17 +2,19 @@ const express = require('express');
 const bodyParser= require('body-parser')
 const MongoClient = require('mongodb').MongoClient
 const app = express();
+const ObjectID = require('mongodb').ObjectID;
 app.set('view engine', 'ejs'); // générateur de template «ejs»
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static('public'))  // pour utiliser le dossier public
 
 var db // variable qui contiendra le lien sur la BD
 
+//127.0.0.1 est équivalent à localhost
 MongoClient.connect('mongodb://127.0.0.1:27017/carnet-adresse', (err, database) => {
   if (err) return console.log(err)
   db = database
   app.listen(8081, () => {
-    console.log('connexion à la BD et on écoute sur le port 8081')
+    console.log('connexion à la BD et on écoute sur le port 8081')//confirmation de la connexion réussie
   })
 })
 
@@ -27,20 +29,20 @@ app.get('/',  (req, res) => {
        if (err) return console.log(err)
     // renders index.ejs
     // affiche le contenu de la BD
-    res.render('index.ejs', {adresse: resultat})
+    res.render('index_1.ejs', {adresse: resultat})//récupère les données du ul adresse
 
     }) 
     
 
 })
 
-
+//affiche le formulaire qui permet de saisir le prenom,nom,telephone,ville,code postal
 app.get('/formulaire',  (req, res) => {
    console.log('la route  get / = ' + req.url)
    res.sendFile(__dirname + "/public/html/forme.htm")
 })
 
-
+//permet d'enregistrer les données entrées dans la base de données
 app.post('/adresse',  (req, res) => {
   db.collection('adresse').save(req.body, (err, result) => {
       if (err) return console.log(err)
@@ -49,13 +51,25 @@ app.post('/adresse',  (req, res) => {
     })
 })
 
-app.get('/detruire/:telephone', (req, res) =>{//faire avec adresse aussi (id) + modifier
+//détruire une entrée
+app.get('/detruire/:id', (req, res) => {
+ var id = req.params.id
+ console.log(id)
+ db.collection('adresse').findOneAndDelete({"_id": ObjectID(req.params.id)}, (err, resultat) => {
 
-  db.collection('adresse').findOneAndDelete({'telephone':req.params.telephone},(err, resultat) =>{
-       if (err) return res.send(500,err)
-       var cursor = db.collection('adresse').find().toArray(function(err, resultat){
-         if (err) return console.log(err)
-         res.render('index_1.ejs', {adresse: resultat})
-       }) 
-    })
+if (err) return console.log(err)
+ res.redirect('/')  // redirige vers la route qui affiche la collection
+ })
+})
+
+//modifier une entrée
+app.post('/modifier', (req, res) => {
+var id = req.params.id
+ //console.log(id)
+ console.log(req.body)
+ db.collection('adresse').findOneAndUpdate({"_id": ObjectID(req.body._id)}, {"prenom" : req.body.prenom, "nom" : req.body.nom, "telephone" : req.body.telephone, "ville" : req.body.ville, "codepostal" : req.body.codepostal}, (err, resultat) => {
+
+if (err) return console.log(err)
+ res.redirect('/')  // redirige vers la route qui affiche la collection
+ })
 })
